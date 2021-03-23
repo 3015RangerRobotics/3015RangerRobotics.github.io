d1ebbe3d0cb3886c790fb79f934bae3311fac458
Michael Jansen <mjansen4857@gmail.com>
2021-03-22 21:43:05 -0400

    add link functionality for reveal videos

--- irh_leaderboard/flutter_service_worker.js
+++ irh_leaderboard/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "df54e16b0420ddb80a97c9032e017dc0",
-"index.html": "2f76972f322162f4a69b3cbcc77fa51f",
-"/": "2f76972f322162f4a69b3cbcc77fa51f",
-"main.dart.js": "d44f5b829fc87b5a13449f354947e63d",
+"index.html": "6dac33bfcbe9a96258a22a1b9cb8bbbb",
+"/": "6dac33bfcbe9a96258a22a1b9cb8bbbb",
+"main.dart.js": "8e57e2ebd03c08b6787a63141b20e17a",
 "favicon.png": "246cfb44c64b6a6120e8aa1b107e791b",
 "icons/Icon-192.png": "e59b3547be77cef801388ba2357e5f8b",
 "icons/Icon-512.png": "8c955f507aa8d16f238b6d49db9ed06d",
